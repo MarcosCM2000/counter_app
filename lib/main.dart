242f4fb95504e1ffcc3c06bba7cacdae46e5378a
MarcosCInland <marcos.andres@inlandlogistics.co>
2022-11-28 10:07:14 -0600

    prefer const

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Myapp());
+  runApp(const Myapp());
 }
 
 class Myapp extends StatelessWidget {
+  const Myapp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
